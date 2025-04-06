13698708eae57bd5a2fbb7448ec465b36d2d5e81
aman-khandelwal-28 <khandelwalaman28@gmail.com>
2025-04-06 15:12:14 +0530

    Logic about how thread works with a join method
    Output:
    Main thread started
    Main thread is waiting for thread1 to finish now
    Thread1 is calling produce method
    Lock acquired
    Lock release
    Main thread ends here

--- src/JoinConcept/Main.java
+++ src/JoinConcept/Main.java
@@ -14,6 +14,15 @@ public class Main {
 
         t1.start();
 
+        // I don't want main thread (current thread) to finish before thread1
+        // I want it to wait for thread1 to complete its task
+        // then main thread should terminate
+        try {
+            System.out.println("Main thread is waiting for thread1 to finish now");
+            t1.join(); // main thread is blocked here and activate again when thread1 completed its task
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("Main thread ends here");
     }
 }
