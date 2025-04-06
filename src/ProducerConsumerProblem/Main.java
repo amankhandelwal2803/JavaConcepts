bd321410d1bf9aaed7e9d1026acc3da476da486c
aman-khandelwal-28 <khandelwalaman28@gmail.com>
2025-04-06 14:06:02 +0530

    Added ProducerConsumer Problem in Multithreading in Java

---
+++ src/ProducerConsumerProblem/Main.java
@@ -0,0 +1,34 @@
+package ProducerConsumerProblem;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SharedResource sharedResource = new SharedResource(3); // shared object
+
+        // creating producer thread using the lambda expression
+        Thread producerThread = new Thread(() -> {
+           try {
+               for(int i = 1; i <= 6; i++) { // 6 items
+                   sharedResource.produce(i);
+               }
+           }catch (Exception e) {
+               e.printStackTrace();
+           }
+        });
+
+        // creating consumer thread using lambda exp
+        Thread consumerThread = new Thread(() -> {
+            try {
+                for(int i = 1; i <= 6; i++) { // 6 items
+                    sharedResource.consume();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        producerThread.start();
+        consumerThread.start();
+    }
+}
---
+++ src/ProducerConsumerProblem/SharedResource.java
@@ -0,0 +1,45 @@
+package ProducerConsumerProblem;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SharedResource {
+
+    private Queue<Integer> sharedBuffer;
+    private int bufferSize;
+
+    public SharedResource(int bufferSize) {
+        sharedBuffer = new LinkedList<>(); // Used LinkedList here because it is unbounded means no size
+        this.bufferSize = bufferSize;
+    }
+
+    public synchronized void produce(int item) throws Exception {
+
+        // If buffer is full, wait for the consumer to consume the items
+        while (sharedBuffer.size() == bufferSize) {
+            System.out.println("Buffer is full, producer is waiting for consumer");
+            wait();
+        }
+
+        sharedBuffer.add(item);
+        System.out.println("Produced: " + item);
+        // notify the consumer that there are items to consume now
+        notify();
+    }
+
+    public synchronized void consume() throws Exception {
+
+        // if buffer is empty, wait for the producer to produce items
+        while (sharedBuffer.isEmpty()) {
+            System.out.println("Buffer is empty, consume is waiting for producer");
+            wait();
+        }
+
+        int item = sharedBuffer.poll();
+        System.out.println("Consumed: " + item);
+
+        // notify the producer that there is space in the buffer now
+        // there might be some producer thread is waiting
+        notify();
+    }
+}
