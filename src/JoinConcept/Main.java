044f184cb60744a8052836c870b975ae12e138ab
aman-khandelwal-28 <khandelwalaman28@gmail.com>
2025-04-06 15:04:06 +0530

    Logic about how thread works without a join method
    Output:
    Main thread started
    Main thread ends here
    Thread1 is calling produce method
    Lock acquired
    Lock release

---
+++ src/JoinConcept/Main.java
@@ -0,0 +1,19 @@
+package JoinConcept;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SharedResource sharedResource = new SharedResource();
+        System.out.println("Main thread started");
+
+        Thread t1 = new Thread(() -> {
+            System.out.println("Thread1 is calling produce method");
+            sharedResource.produce();
+        });
+
+        t1.start();
+
+        System.out.println("Main thread ends here");
+    }
+}
