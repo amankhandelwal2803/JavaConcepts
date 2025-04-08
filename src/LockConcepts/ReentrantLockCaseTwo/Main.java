9adb46ef9d024aeff135c7a4bd8689d0e1a5b4ae
aman-khandelwal-28 <khandelwalaman28@gmail.com>
2025-04-08 20:55:36 +0530

    ReentrantLock concept - case 2
    Output:
    Lock acquired by: Thread-0
    Lock release by: Thread-0
    Lock acquired by: Thread-1
    Lock release by: Thread-1
    
    Process finished with exit code 0

---
+++ src/LockConcepts/ReentrantLockCaseTwo/Main.java
@@ -0,0 +1,22 @@
+package LockConcepts.ReentrantLockCaseTwo;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // two different objects with same lock
+        // reentrantLock do not depend on one object, it just depends upon a lock
+        ReentrantLock reentrantLock = new ReentrantLock();
+
+        SharedResource resource1 = new SharedResource();
+        Thread t1 = new Thread(() -> resource1.produce(reentrantLock));
+
+        SharedResource resource2 = new SharedResource();
+        Thread t2 = new Thread(() -> resource2.produce(reentrantLock));
+
+        t1.start();
+        t2.start();
+    }
+}
---
+++ src/LockConcepts/ReentrantLockCaseTwo/SharedResource.java
@@ -0,0 +1,24 @@
+package LockConcepts.ReentrantLockCaseTwo;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SharedResource {
+
+    boolean isAvailable = false;
+
+    public void produce(ReentrantLock reentrantLock) {
+        try {
+            reentrantLock.lock(); // acquire the lock
+            System.out.println("Lock acquired by: " + Thread.currentThread().getName());
+            isAvailable = true;
+            Thread.sleep(4000L);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally { // this finally should execute always even if an exception occurs
+            System.out.println("Lock release by: " + Thread.currentThread().getName());
+            reentrantLock.unlock(); // release the lock
+        }
+    }
+}
