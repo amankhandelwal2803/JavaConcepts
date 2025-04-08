79fbe19112d0b169daa5b7fed5b6690c917f90b6
aman-khandelwal-28 <khandelwalaman28@gmail.com>
2025-04-08 22:22:51 +0530

    ReadWriteLock concept
    Output:
    Read lock acquired by: Thread-1
    Read lock acquired by: Thread-0
    Read lock release by: Thread-1
    Read lock release by: Thread-0
    Write lock acquired by: Thread-2
    Write lock release by: Thread-2
    
    Process finished with exit code 0

---
+++ src/LockConcepts/ReadWriteLock/Main.java
@@ -0,0 +1,27 @@
+package LockConcepts.ReadWriteLock;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SharedResource resource1 = new SharedResource();
+        ReadWriteLock lock = new ReentrantReadWriteLock();
+
+        // both threads are allowed to acquire the lock
+        Thread t1 = new Thread(() -> resource1.produce(lock)); // Shared lock on same resource
+        Thread t2 = new Thread(() -> resource1.produce(lock)); // Shared lock on same resource
+
+        SharedResource resource2 = new SharedResource();
+
+        // object is different but lock is same
+        // lock is not granted here because lock is already acquired by t1 and t2
+        Thread t3 = new Thread(() -> resource2.consume(lock)); // Exclusive lock
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
---
+++ src/LockConcepts/ReadWriteLock/SharedResource.java
@@ -0,0 +1,39 @@
+package LockConcepts.ReadWriteLock;
+
+import java.util.concurrent.locks.ReadWriteLock;
+
+public class SharedResource {
+
+    boolean isAvailable = false;
+
+    public void produce(ReadWriteLock lock) {
+        try {
+            lock.readLock().lock();
+            System.out.println("Read lock acquired by: " + Thread.currentThread().getName());
+            // read value only - isAvailable
+            Thread.sleep(8000L);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            lock.readLock().unlock();
+            System.out.println("Read lock release by: " + Thread.currentThread().getName());
+        }
+    }
+
+    public void consume(ReadWriteLock lock) {
+        try {
+            lock.writeLock().lock();
+            System.out.println("Write lock acquired by: " + Thread.currentThread().getName());
+            isAvailable = false;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            lock.writeLock().unlock();
+            System.out.println("Write lock release by: " + Thread.currentThread().getName());
+        }
+    }
+}
